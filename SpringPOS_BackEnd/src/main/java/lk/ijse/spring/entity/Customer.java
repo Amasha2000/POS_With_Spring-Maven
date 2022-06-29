6ce8919c2d0cda2aeb527bc46235c253b7a2e28a
Amasha2000 <amashagalhenage@gmail.com>
2022-06-30 00:45:12 +0530

    Added details to Customer class

--- SpringPOS_BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
+++ SpringPOS_BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,5 +1,23 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String customerID;
+    private String customerName;
+    private String customerAddress;
+    private String customerTeleNumber;
 
 }
