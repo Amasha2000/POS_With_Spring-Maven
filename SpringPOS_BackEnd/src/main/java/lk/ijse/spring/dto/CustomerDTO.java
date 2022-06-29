bac544f153740dff5f320ff45c278ef72b2dd6ab
Amasha2000 <amashagalhenage@gmail.com>
2022-06-30 00:40:36 +0530

    Added details to CustomerDTO

--- SpringPOS_BackEnd/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ SpringPOS_BackEnd/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CustomerDTO {
+    private String customerID;
+    private String customerName;
+    private String customerAddress;
+    private String customerTeleNumber;
 }
