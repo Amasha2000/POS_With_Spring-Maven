ddc886d7f8bb0d3eb62b591031b29bc62b73571d
Amasha2000 <amashagalhenage@gmail.com>
2022-07-02 20:43:10 +0530

    Content Updated

--- SpringPOS_BackEnd/src/main/java/lk/ijse/spring/repo/OrderDetailRepo.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.spring.repo;
-
-import lk.ijse.spring.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderDetailRepo extends JpaRepository<OrderDetail,String> {
-}
--- SpringPOS_BackEnd/src/main/java/lk/ijse/spring/service/PlaceOrderService.java
+++
@@ -1,8 +0,0 @@
-package lk.ijse.spring.service;
-
-import lk.ijse.spring.dto.OrderDTO;
-
-public interface PlaceOrderService {
-    void placeOrder(OrderDTO orderDTO);
-    String getAutoOrderId();
-}
